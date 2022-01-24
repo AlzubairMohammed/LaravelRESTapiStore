628a8183236b7a320ca1b63ee28d1e8acaba5849
AlzubairMohammed <alzubairm900@gmail.com>
2022-01-23 22:06:48 -0800

    frist addition

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\products;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $products = products::all() ;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        products::create([
+            'name'=>$request->name,
+            'price'=>$request->price,
+            'description'=>$request->description,
+            'categories'=>$request->categories,
+            'categoriesValue'=>$request->categoriesValue
+        ]);
+        return 201 ;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return products::find($id) ;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(products $products)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $products = products::findOrFail($request->id);
+
+       $products->update([
+       'name' => $request->name,
+       'price' => $request->price,
+       'description' => $request->description,
+       'categories' => $request->categories,
+       'categoriesValue' => $request->categoriesValue
+       ]);
+
+       return $request;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        products::destroy($request->id) ;
+
+        return 200;
+    }
+}
